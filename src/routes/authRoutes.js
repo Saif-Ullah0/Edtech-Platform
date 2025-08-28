1823d431ab2f45e28135cbe27744a31a5637c4fb
Saif Ullah <saifullaharshad110@gmail.com>
2025-08-28 19:23:53 +0500

    added Google auth route , Google callback route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const { register, login, logout, googleAuth, googleAuthCallback, facebookAuth, facebookAuthCallback } = require('../controllers/authController');
+
 const router = express.Router();
-const { register,login,logout,  } = require('../controllers/authController');
-const requireAuth = require('../middlewares/requireAuth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
-//router.get('/me', requireAuth, getCurrentUser);
+router.get('/google', googleAuth);              // New: Google auth route
+router.get('/google/callback', googleAuthCallback); // New: Google callback route
+router.get('/facebook', facebookAuth);          // New: Facebook auth route
+router.get('/facebook/callback', facebookAuthCallback); // New: Facebook callback route
 
 module.exports = router;
